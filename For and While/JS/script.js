69995419ab8676cc4cb4b66c762432b77a0d0bf4
Ryuu <132589879+D3vilRyuu@users.noreply.github.com>
2023-08-01 14:25:41 -0300

    Add files via upload

---
+++ For and While/JS/script.js
@@ -0,0 +1,18 @@
+var expressao = document.getElementById("tabuada").value
+var adicao = document.getElementsByClassName("")
+function tabuada() {
+
+
+    switch (expressao){
+
+        case 'adicao':
+
+    }
+    
+}
+
+for (contador = 0; contador <= 10; contador++) {
+
+
+
+    }
